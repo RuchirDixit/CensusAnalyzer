0ac13a36b8ccc4dd2364d8a460ff21d24d12f1cc
RuchirDixit <ruchirtd96@gmail.com>
2021-02-18 01:40:49 +0530

    [Ruchir]Added: Test cases for Uc2

--- src/test/scala/com/cenusAnalyzerTest/CensusAnalyzerTest.scala
+++ src/test/scala/com/cenusAnalyzerTest/CensusAnalyzerTest.scala
@@ -15,9 +15,11 @@
 // limitations under the License.
 package com.cenusAnalyzerTest
 
-import com.censusAnalyzer.CensusAnalyzer
+import com.censusAnalyzer.IndiaStateCensusDataAnalyser.loadIndiaStateCensusData
+import com.censusAnalyzer.IndiaStateCodeAnalyser.loadIndiaStateCode
 import com.censusAnalyzer.exception.CensusAnalyzerException
 import com.censusAnalyzer.exception.CensusAnalyzerException.Issue
+
 import org.scalatest.FunSuite
 
 /***
@@ -31,28 +33,52 @@ class CensusAnalyserTest extends FunSuite {
   val wrongFilePathForIndiaStateCensusData = "./IndiaStateCensusData.csv"
   val wrongTypeOfIndiaStateCensusData = "C:\\Users\\Ruchir Dixit\\IdeaProjects\\Census_Analyzer\\src\\test\\Resources\\IndiaStateCensusData.txt"
   val wrongHeaderIndiaStateCensusPath = "C:\\Users\\Ruchir Dixit\\IdeaProjects\\Census_Analyzer\\src\\test\\Resources\\IndiaStateCodeWrongHeader.csv"
-  val CensusAnalyzerObj = new CensusAnalyzer()
-
+  //path variables for IndiaStateCode
+  val indiaStateCodeDataPath = "C:\\Users\\Ruchir Dixit\\IdeaProjects\\Census_Analyzer\\src\\test\\Resources\\IndiaStateCode.csv"
+  val wrongIndiaStateCodeFilePath = "C:\\Users\\IdeaProjects\\Census_Analyzer\\src\\test\\Resources\\IndiaStateCode.csv"
+  val wrongFileTypeIndiaStateCodePath = "C:\\Users\\Ruchir Dixit\\IdeaProjects\\Census_Analyzer\\src\\test\\Resources\\IndiaStateCode.pdf"
+  val wrongHeaderIndiaStateCodePath = "C:\\Users\\Ruchir Dixit\\IdeaProjects\\Census_Analyzer\\src\\test\\Resources\\IndiaStateCodeWrongHeader.csv"
   test("givenIndianCensusCSVFileShouldReturnCorrectNumberOfRecords") {
-    assert(CensusAnalyzerObj.loadCSVData(indiaStateCensusDataPath) === 29)
+    assert(loadIndiaStateCensusData(indiaStateCensusDataPath) === 29)
   }
-
   test("givenIndianCensusDataCSVFileIfWrongFilePathShouldThrowException") {
     val thrown = intercept[CensusAnalyzerException] {
-      CensusAnalyzerObj.loadCSVData(wrongFilePathForIndiaStateCensusData)
+      loadIndiaStateCensusData(wrongFilePathForIndiaStateCensusData)
     }
     assert(thrown.getMessage === Issue.PATH_INCORRECT)
   }
   test("givenIndianCensusDataFileIfWrongTypeShouldThrowException") {
-    val thrown = intercept[Exception] {
-      CensusAnalyzerObj.loadCSVData(wrongTypeOfIndiaStateCensusData)
+    val thrown = intercept[CensusAnalyzerException] {
+      loadIndiaStateCensusData(wrongTypeOfIndiaStateCensusData)
     }
-    assert(thrown.getMessage === Issue.INCORRECT_FILE.toString)
+    assert(thrown.getMessage === Issue.INCORRECT_FILE)
   }
   test("givenIndianCensusDataFileIfWrongHeaderShouldThrowException") {
-    val thrown = intercept[Exception] {
-      CensusAnalyzerObj.loadCSVData(wrongHeaderIndiaStateCensusPath)
+    val thrown = intercept[CensusAnalyzerException] {
+      loadIndiaStateCensusData(wrongHeaderIndiaStateCensusPath)
     }
-    assert(thrown.getMessage === Issue.INVALID_FIELDS.toString)
+    assert(thrown.getMessage === Issue.INVALID_FIELDS)
+  }
+
+  test("givenIndiaStateCodeCSVFileShouldReturnCorrectNumberOfRecords") {
+    assert(loadIndiaStateCode(indiaStateCodeDataPath) === 37)
+  }
+  test("givenIndiaStateCodeCSVFileIfWrongFilePathShouldThrowException") {
+    val thrown = intercept[CensusAnalyzerException] {
+      loadIndiaStateCode(wrongIndiaStateCodeFilePath)
+    }
+    assert(thrown.getMessage === Issue.PATH_INCORRECT)
+  }
+  test("givenIndiaStateCodeFileIfWrongTypeShouldThrowException") {
+    val thrown = intercept[CensusAnalyzerException] {
+      loadIndiaStateCode(wrongFileTypeIndiaStateCodePath)
+    }
+    assert(thrown.getMessage === Issue.INCORRECT_FILE)
+  }
+  test("givenIndiaStateCodeFileIfWrongHeaderShouldThrowException") {
+    val thrown = intercept[CensusAnalyzerException] {
+      loadIndiaStateCode(wrongHeaderIndiaStateCodePath)
+    }
+    assert(thrown.getMessage === Issue.INVALID_FIELDS)
   }
 }

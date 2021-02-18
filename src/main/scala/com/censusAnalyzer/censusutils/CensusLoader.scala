b074e759c2152fdcff70ceac362653a72abc322c
RuchirDixit <ruchirtd96@gmail.com>
2021-02-19 00:16:02 +0530

    [Ruchir]Refactored: Introduced single responsibility principle

--- src/main/scala/com/censusAnalyzer/censusutils/CensusLoader.scala
+++ src/main/scala/com/censusAnalyzer/censusutils/CensusLoader.scala
@@ -1,4 +1,18 @@
-
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.censusAnalyzer.censusutils
 
 import java.nio.file.{Files, NoSuchFileException, Paths}
@@ -8,6 +22,13 @@ import com.censusAnalyzer.exception.CensusAnalyzerException.Issue
 import scala.io.Source
 
 object CensusLoader {
+  /**
+   * To Load CSV data
+   * @param filePath : Path of the file
+   * @param headers : Header passed
+   * @throws CensusAnalyzerException : when required thrown
+   * @return : util.ArrayList[util.ArrayList[String]]
+   */
   @throws[CensusAnalyzerException]
   @Deprecated
   def loadCSVData(filePath: String, headers: Array[String]): util.ArrayList[util.ArrayList[String]] = {
@@ -46,6 +67,11 @@ object CensusLoader {
     }
   }
 
+  /**
+   *  To chec file properties
+   * @param filePath : Path of the file passed
+   * @param headers : Header passed
+   */
   def checkFileProperties(filePath: String, headers: Array[String]): Unit = {
     if (!filePath.endsWith(".csv")) {
       throw new CensusAnalyzerException(Issue.INCORRECT_FILE)

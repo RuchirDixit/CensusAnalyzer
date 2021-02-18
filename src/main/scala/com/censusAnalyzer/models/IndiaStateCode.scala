908acb032b4458a2954693284f5028270d574f3b
RuchirDixit <ruchirtd96@gmail.com>
2021-02-19 00:19:18 +0530

    [Ruchir]Added: Comments

--- src/main/scala/com/censusAnalyzer/models/IndiaStateCode.scala
+++ src/main/scala/com/censusAnalyzer/models/IndiaStateCode.scala
@@ -1,3 +1,18 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.censusAnalyzer.models
 
 import com.opencsv.bean.CsvBindByName
@@ -8,8 +23,14 @@ class IndiaStateCode {
   @CsvBindByName(column = "TIN", required = true) var tin: Int = 0
   @CsvBindByName(column = "StateCode", required = true) var stateCode: String = _
 
-  override def toString: String = "StateCode{" + "SrNo='" + srNo + '\'' + ", state='" + state + '\'' + ", TIN='" + tin + '\'' + ", stateCode='" + stateCode + '\'' + '}'
+  override def toString: String = "StateCode{" +
+    "SrNo='" + srNo + '\'' + ", state='" + state + '\'' + ", TIN='" + tin + '\'' + ", stateCode='" + stateCode + '\'' + '}'
 
+  /**
+   * To get data
+   * @param column : index
+   * @return ; Object
+   */
   def get(column: Int):Object = {
     column match{
       case 0 => srNo.asInstanceOf[Object]

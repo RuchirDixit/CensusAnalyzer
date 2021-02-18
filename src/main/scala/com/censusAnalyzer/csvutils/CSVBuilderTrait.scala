d8b79b69afd63db794e468f27aa3e096d990b7a4
RuchirDixit <ruchirtd96@gmail.com>
2021-02-18 13:37:30 +0530

    [Ruchir]Added: CsvBuilder interface

---
+++ src/main/scala/com/censusAnalyzer/csvutils/CSVBuilderTrait.scala
@@ -0,0 +1,9 @@
+package com.censusAnalyzer.csvutils
+
+import java.io.Reader
+import java.util
+
+trait CSVBuilderTrait {
+  def getIterator[T](reader: Reader, csvClass: Class[T]): util.Iterator[T]
+  def fetchList[T](reader: Reader, csvClass: Class[T]): util.List[T]
+}
